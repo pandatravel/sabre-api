1bf9814ba72eb1de9ab210675b3884aecc7710fa
Ammon Casey <acasey@panda-group.com>
2019-06-10 15:05:38 -1000

    [model] adds some comments

--- src/Model/BargainFinderMax/BargainFinderMaxRequest.php
+++ src/Model/BargainFinderMax/BargainFinderMaxRequest.php
@@ -7,14 +7,24 @@ use Ammonkc\SabreApi\AbstractModel;
 class BargainFinderMaxRequest extends AbstractModel
 {
     /**
-     * The Low Fare Search Request message requests priced itinerary options for flights between specific city pairs on specific dates for specific numbers and types of passengers. Optional request information can include: - Time / Time Window - Connecting cities. - Client Preferences (airlines, cabin, flight types etc.) The Low Fare Search request contains similar information to a Low Fare Search entry on an airline CRS or GDS.
+     * The Low Fare Search Request message requests priced itinerary options for
+     * flights between specific city pairs on specific dates for specific
+     * numbers and types of passengers. Optional request information can
+     * include: - Time / Time Window - Connecting cities. - Client Preferences
+     * (airlines, cabin, flight types etc.) The Low Fare Search request contains
+     * similar information to a Low Fare Search entry on an airline CRS or GDS.
      *
      * @var BargainFinderMaxRequestOTAAirLowFareSearchRQ
      */
     protected $oTAAirLowFareSearchRQ;
 
     /**
-     * The Low Fare Search Request message requests priced itinerary options for flights between specific city pairs on specific dates for specific numbers and types of passengers. Optional request information can include: - Time / Time Window - Connecting cities. - Client Preferences (airlines, cabin, flight types etc.) The Low Fare Search request contains similar information to a Low Fare Search entry on an airline CRS or GDS.
+     * The Low Fare Search Request message requests priced itinerary options for
+     * flights between specific city pairs on specific dates for specific
+     * numbers and types of passengers. Optional request information can
+     * include: - Time / Time Window - Connecting cities. - Client Preferences
+     * (airlines, cabin, flight types etc.) The Low Fare Search request contains
+     * similar information to a Low Fare Search entry on an airline CRS or GDS.
      *
      * @return BargainFinderMaxRequestOTAAirLowFareSearchRQ
      */
@@ -24,7 +34,12 @@ class BargainFinderMaxRequest extends AbstractModel
     }
 
     /**
-     * The Low Fare Search Request message requests priced itinerary options for flights between specific city pairs on specific dates for specific numbers and types of passengers. Optional request information can include: - Time / Time Window - Connecting cities. - Client Preferences (airlines, cabin, flight types etc.) The Low Fare Search request contains similar information to a Low Fare Search entry on an airline CRS or GDS.
+     * The Low Fare Search Request message requests priced itinerary options for
+     * flights between specific city pairs on specific dates for specific
+     * numbers and types of passengers. Optional request information can
+     * include: - Time / Time Window - Connecting cities. - Client Preferences
+     * (airlines, cabin, flight types etc.) The Low Fare Search request contains
+     * similar information to a Low Fare Search entry on an airline CRS or GDS.
      *
      * @param BargainFinderMaxRequestOTAAirLowFareSearchRQ $oTAAirLowFareSearchRQ
      *
--- src/Model/BargainFinderMax/BargainFinderMaxRequestOTAAirLowFareSearchRQ.php
+++ src/Model/BargainFinderMax/BargainFinderMaxRequestOTAAirLowFareSearchRQ.php
@@ -4,6 +4,15 @@ namespace Ammonkc\SabreApi\Model\BargainFinderMax;
 
 use Ammonkc\SabreApi\AbstractModel;
 
+/**
+ * The Low Fare Search Request message requests priced itinerary options for
+ * flights between specific city pairs on specific dates for specific
+ * numbers and types of passengers. Optional request information can
+ * include: - Time / Time Window - Connecting cities. - Client Preferences
+ * (airlines, cabin, flight types etc.) The Low Fare Search request contains
+ * similar information to a Low Fare Search entry on an airline CRS or GDS.
+ *
+ */
 class BargainFinderMaxRequestOTAAirLowFareSearchRQ extends AbstractModel
 {
     /**
